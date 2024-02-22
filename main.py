a8c448119705608b5966bd257b2a598682a19537
Akanksha Kailash Patil <63913659+akanksha-patil1404@users.noreply.github.com>
2024-02-23 00:06:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import cv2
+from cvzone.PoseModule import PoseDetector
+detector=PoseDetector()
 
-# Function to calculate direction of movement
+
+
+
 def calculate_direction(old_points, new_points):
     direction = "None"
     if old_points is not None and new_points is not None:
@@ -16,12 +20,16 @@ def calculate_direction(old_points, new_points):
             direction = "Up"
     return direction
 
-# Function to detect human bodies using Haar cascade and track back and forth movement
+
+
 def detect_and_track(video_path):
     # Load pre-trained Haar cascade classifier for human body detection
     body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+    # Open video capture
     cap = cv2.VideoCapture(video_path)
 
+    # Initialize variables for direction tracking
     prev_points = None
     movement_count = 0
     suspicious_detected = False
@@ -30,9 +38,17 @@ def detect_and_track(video_path):
         ret, frame = cap.read()
         if not ret:
             break
+
+
+        frame = detector.findPose(frame)
+        lmlist, bboxInfo = detector.findPosition(frame, bboxWithHands=True)
+        # Convert frame to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect human bodies in the frame
         bodies = body_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
+        # Draw bounding boxes around detected human bodies and track back and forth movement
         for (x, y, w, h) in bodies:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
@@ -53,14 +69,21 @@ def detect_and_track(video_path):
                     suspicious_detected = True
             else:
                 movement_count = 0
+
+            # Update previous points
             prev_points = current_point
 
+        # Display frame with bounding boxes
         cv2.imshow('Frame', frame)
+
+        # Exit if 'q' is pressed or suspicious movement is detected
         if cv2.waitKey(30) & 0xFF == ord('q') or suspicious_detected:
             break
 
+    # Release video capture and close all windows
     cap.release()
     cv2.destroyAllWindows()
 
-video_path = 'video.mp4'
+# Example usage
+video_path = ('vd1.mp4')
 detect_and_track(video_path)
